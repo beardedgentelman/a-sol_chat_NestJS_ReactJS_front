5d32cea1d62fed7f4190cf63943bcac895badeb1
Andrii <girskiy.andrey@gmail.com>
2023-06-05 10:03:05 +0300

    uncomment

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import ChatsPage from 'pages/ChatsPage/ChatsPage'
 import RegistrationPage from 'pages/RegistrationPage/RegistrationPage'
 import { Navigate, Route, Routes } from 'react-router-dom'
+import AuthRoute from 'routes/AuthRoute'
 
 function App() {
   return (
@@ -8,9 +9,9 @@ function App() {
       <Routes>
         {/* TODO: Make private routes */}
 
-        {/* <Route element={<AuthRoute />}> */}
-        <Route path='/chats' element={<ChatsPage />} />
-        {/* </Route> */}
+        <Route element={<AuthRoute />}>
+          <Route path='/chats' element={<ChatsPage />} />
+        </Route>
         <Route path='/' element={<Navigate to='/registration' replace />} />
         <Route path='/registration' element={<RegistrationPage />} />
       </Routes>
