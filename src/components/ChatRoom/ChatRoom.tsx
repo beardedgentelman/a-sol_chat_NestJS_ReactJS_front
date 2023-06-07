76394aa963ae6b5964ac704d65f975bcf49b4a45
Andrii <girskiy.andrey@gmail.com>
2023-06-07 12:40:32 +0300

    added Diexie for work with IndexedDB and saving messages to Messages DB in the IndexedDB

--- src/components/ChatRoom/ChatRoom.tsx
+++ src/components/ChatRoom/ChatRoom.tsx
@@ -1,15 +1,20 @@
 import BtnMain from 'components/ui/BtnMain/BtnMain'
+import MessageCard from 'components/ui/MessageCard/MessageCard'
+import { useLiveQuery } from 'dexie-react-hooks'
+import { addMessageToIndexDb, messagesTableIndexedDb } from 'helpers/toIndexDB'
 import { useAppDispatch, useAppSelector } from 'hooks/redux'
-import { useEffect, useRef } from 'react'
+import { FC, useEffect, useRef } from 'react'
 import { setMessage } from 'store/reducers/messagesSlice'
 import { IMessageState } from 'types/types'
 import './chat-room.css'
 
-const ChatRoom = () => {
+const ChatRoom: FC = () => {
   const textareaRef = useRef<HTMLTextAreaElement>(null)
 
   const dispatch = useAppDispatch()
   const { user } = useAppSelector(state => state.userReducer)
+
+  const allMessages = useLiveQuery(() => messagesTableIndexedDb.toArray(), [])
 
   useEffect(() => {
     const textarea = textareaRef.current
@@ -34,21 +39,36 @@ const ChatRoom = () => {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
 
-    const messageText = textareaRef.current?.value
+    const messageText: string | null = textareaRef.current?.value ?? null
 
     const message: IMessageState = {
       text: messageText,
       user: user,
-      date: new Date().toISOString()
+      date: new Date().toUTCString()
     }
 
+    addMessageToIndexDb(message)
     dispatch(setMessage(message))
+    e.currentTarget.reset()
   }
 
   return (
     <>
-      <div className='chat-room__messages'></div>
-      <form id='chat__form' className='chat-room__form' onSubmit={handleSubmit}>
+      <div className='chat-room__messages'>
+        {allMessages?.map(({ text, user, date }, id) => (
+          <MessageCard text={text} user={user} date={date} key={id} />
+        ))}
+      </div>
+      <form
+        id='chat__form'
+        className='chat-room__form'
+        onSubmit={handleSubmit}
+        onKeyDown={ev => {
+          if (ev.key === 'Enter') {
+            handleSubmit(ev)
+          }
+        }}
+      >
         <div className='form__textarea-container'>
           <textarea
             ref={textareaRef}
